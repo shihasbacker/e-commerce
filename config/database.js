bdcc8a0cb6f67c3bc3ca94f8694312c92cbe0b70
shihasbacker <shihasbacker@yahoo.com>
2022-10-19 14:31:40 +0530

    mongo atlas added

--- config/database.js
+++ config/database.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
 
 
-const connectDatabase = mongoose.connect(
-  "mongodb://localhost:27017/e-commerce",
-  {
-    // useUnifiedTopology: true,
-    // useNewUrlParser: true,
-    // useCreateIndex: true, //make this true
-    // autoIndex: true, //make this also true
-}).then(() => {
-    console.log('Connected to mongoDB');
-}).catch((e)=>{
-  console.log(e)
+// const connectDatabase = mongoose.connect(
+//   "mongodb://localhost:27017/e-commerce",
+//   {
+//     // useUnifiedTopology: true,
+//     // useNewUrlParser: true,
+//     // useCreateIndex: true, //make this true
+//     // autoIndex: true, //make this also true
+// }).then(() => {
+//     console.log('Connected to mongoDB');
+// }).catch((e)=>{
+//   console.log(e)
+// })
+
+const connectDatabase=process.env.MONGO_CONNECTION_ID
+mongoose.connect(connectDatabase, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err) => {
+    if (err) return console.log('err connection', err);
+    console.log('database connected');
 })
+
+
+
 
 module.exports = connectDatabase;
